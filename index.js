ecb270e597f85b836c57c2c76b9dac599766b974
Rich Matthews <richmatthews@notonthehighstreet.com>
2018-05-31 11:02:28 +0100

    Add items to dynamo via lambda

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-function test(){
+const AWS = require("aws-sdk");
+let ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+exports.myLambda = function(event, context, callback){
   console.log('testing that lambda works');
+  console.log(event, 'this is the event...');
+  var params = {
+    TableName: 'garply-CreateDynamoDBTable-GOWCBPMPOFP5',
+    Item: {
+      'CUSTOMER_ID': {N: '001'},
+      'CUSTOMER_NAME': {S: 'RICHARD M'},
+    }
+  }
+  ddb.putItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
 }
-test()
